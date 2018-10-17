42b35563695b8e2e2bc89776c29c4e153aba2346
Vitalik Parshyn <vitalik.parshin@gmail.com>
2018-10-18 00:57:35 +0300

    Fix layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Game2048 from "./Game2048"
+import Game2048 from './Game2048';
+import { ScrollView, Text, Dimensions } from 'react-native';
+
+const { height } = Dimensions.get('window');
 
 class App extends Component {
   render() {
+    console.log('height', height);
     return (
-      <div className="App">
-        <header className="App-header">
-          <p style={styles.prodName}>© Copyright by Vitalii Parshin</p>
-          <img src={logo} className="App-logo" alt="logo" />
-          <Game2048 />
-          <p style={styles.title}>Game 2048 for Real-men</p>
-        </header>
-      </div>
+      <ScrollView
+        scrollEnabled={false}
+        style={{
+          backgroundColor: '#282c34',
+          height: height
+        }}
+        contentContainerStyle={{
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          height: height
+        }}
+      >
+        <Text style={styles.prodName}>© Copyright by Vitalii Parshin</Text>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Game2048 />
+        <Text style={styles.title}>Game 2048 for Real-men</Text>
+      </ScrollView>
     );
   }
 }
@@ -21,13 +34,19 @@ class App extends Component {
 const styles = {
   title: {
     fontFamily: 'Chalkduster',
-    fontSize: 39,
-    marginBottom: 20
+    fontSize: 25,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 20,
+    marginBottom: 20,
+    color: 'white',
+    textAlign: 'center'
   },
   prodName: {
     fontFamily: 'Chalkduster',
     fontSize: 14,
-    marginBottom: 20
+    marginTop: 20,
+    color: 'white'
   }
 };
 
