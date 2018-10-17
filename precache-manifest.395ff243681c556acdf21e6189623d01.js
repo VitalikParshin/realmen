992629a016a5d77cc92950fe9f407b44bff34430
Vitalik Parshyn <vitalik.parshin@gmail.com>
2018-10-17 23:12:45 +0300

    Updates

---
+++ precache-manifest.395ff243681c556acdf21e6189623d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/realmen/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f6c328f1ff2865f8baf3",
+    "url": "/realmen/static/js/runtime~main.f6c328f1.js"
+  },
+  {
+    "revision": "1439f36acb64678d3cdc",
+    "url": "/realmen/static/js/main.1439f36a.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/realmen/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "1439f36acb64678d3cdc",
+    "url": "/realmen/static/css/main.2775524c.chunk.css"
+  },
+  {
+    "revision": "8fe56fd9b30248e217e747d62e53f714",
+    "url": "/realmen/index.html"
+  }
+];
