c317df6901a2669d3f242992ea2a10996342f1d2
Vitalik Parshyn <vitalik.parshin@gmail.com>
2018-10-17 23:58:16 +0300

    Add 'react-art' 'react-native-web' 'react-native' libraries and 'Game2048' component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game2048 from "./Game2048"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <p style={styles.prodName}>© Copyright by Vitalii Parshin</p>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Game2048 />
+          <p style={styles.title}>Game 2048 for Real-men</p>
         </header>
       </div>
     );
   }
 }
+
+const styles = {
+  title: {
+    fontFamily: 'Chalkduster',
+    fontSize: 39,
+    marginBottom: 20
+  },
+  prodName: {
+    fontFamily: 'Chalkduster',
+    fontSize: 14,
+    marginBottom: 20
+  }
+};
 
 export default App;
